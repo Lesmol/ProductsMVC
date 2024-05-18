c312834b2ea847cdf40154f66c4041cd7777fafd
Lesedi <molemilesedi@gmail.com>
2024-05-18 09:27:57 +0200

    Completed home page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
         './Views/**/*.cshtml'
     ],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                sm: "480px",
+                md: "768px",
+                lg: "976px",
+                xl: "1440px",
+                "2xl": "1536px",
+            },
+            colors: {
+                primary: "#583900",
+                "light-green": "#04FFB4",
+                "light-blue": "#04C3FF",
+                red: "#FF0000",
+                "darker-red": "#D2042D",
+            },
+            backgroundColor: {
+                orange: "#FFA500",
+                "light-green": "#04FFB4",
+                "light-blue": "#04C3FF",
+            },
+            aspectRatio: {
+                "752/444": "752/444",
+            }
+        }
     },
     plugins: [],
 }
